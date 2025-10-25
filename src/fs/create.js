124b5f27cddd5d277e62cdf743cf82d94d2ff3d0
Aliaksei-Varabyou <aleksey.vorobyov@gmail.com>
2025-10-25 20:20:31 +0200

    feat: fs api

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+/*
+* implement function that creates new file fresh.txt with content
+* "I am fresh and young" inside of the files folder 
+* (if file already exists Error with message FS operation failed must be thrown)
+*/
+
+import { writeFile } from 'node:fs/promises';
+import { fileExists } from '../utils/fileExists.js';
+
 const create = async () => {
-  // Write your code here
+  const text = 'I am fresh and young';
+  const path = './src/fs/files/fresh.txt';
+
+  if (await fileExists(path)) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await writeFile(path, text, {encoding: 'utf8'});
+    console.log('File was created');
+  } catch {
+    throw new Error('FS operation failed');
+  }
+
 };
 
 await create();
