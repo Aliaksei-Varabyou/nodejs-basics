5fe882a0be61dfcf9544c49f9e99e09b90098612
Aliaksei-Varabyou <aleksey.vorobyov@gmail.com>
2025-10-26 10:24:38 +0100

    feat: crypto api

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+/*
+* implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt 
+* and logs it into console as hex using Streams API
+*/
+import { createHash } from 'node:crypto';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileExists } from '../utils/fileExists.js';
+
 const calculateHash = async () => {
-  // Write your code here
+
+  const filePath = './src/hash/files/fileToCalculateHashFor.txt';
+  if (!fileExists(filePath)) {
+    throw new Error('FS operation failed');
+  }
+  const reader = createReadStream(filePath);
+  const hash = createHash('sha256');
+  
+  reader.on('data', chunk => {
+    hash.update(chunk);
+  });
+  reader.on('end', () => {
+    console.log('SHA256:', hash.digest('hex'));
+  });
+  reader.on('error', (err) => {
+    console.error('Error:', err);
+  });
 };
 
 await calculateHash();
